811f51e5234ba9ca4f6b9e1d4e5c5d1f521b8f08
Penelope <83740019+penelope366@users.noreply.github.com>
2026-07-20 11:56:41 -0400

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-
+//outdated and doesn't connect to anything, probably need to edit it
 const searchForm = document.getElementById('searchForm');
 const searchInput = document.getElementById('searchInput');
 
